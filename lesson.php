70e69ee800308ac8b6b63c899bf8f9c73a6caf60
romariozav28 <romariozav5@gmail.com>
2022-11-03 08:48:35 +0400

    create file

---
+++ lesson.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'lib.php';
+
+echonl('--== Bulls and Cows ==--');
+echonl('------------------------');
+echonl('');
+
+echo 'Generating a random number .... ';
+
+$secret = mt_rand(1000, 9999);
+
+echonl('Done!');
+
+$saved = file_put_contents('secret.txt', $secret);
+if ($saved == false) {
+    error_log ('Failed to save to "secret.txt".');
+}
+
+echonl("It's a " . $secret . "!");
+
+
